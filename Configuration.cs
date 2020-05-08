a93731465898c403c502c475935b4abf6d3a499b
benjiiman12@gmail.com <benjiiman12@gmail.com>
2020-05-08 20:19:08 +1200

    working vehicle repair with options from config

--- Configuration.cs
+++ Configuration.cs
@@ -3,41 +3,67 @@ using System.IO;
 using System.Windows.Forms;
 
 
-namespace ModTemplate
+// TODO - potential options:
+// show vehicle name?
+// need to clean dirt/snow? - option to leave dirt after repair?
+// clean player option?
+// instant get into any vehicle thats close with engine start
+
+namespace VehicleInstantRepair
 {
     class Configuration
     {
-        ScriptSettings config;
+        private static ScriptSettings config;
+        private string configFile = "scripts\\InstantRepair.ini";
 
-        private string configFile = "scripts\\MyModTemplate.ini";
+        private static string sectionKeys = "KEYS";
+        private static string sectionOptions = "OPTIONS";
 
-        public int Number { get; set; } = 30;
+        // initialise key map defaults
+        private static Keys repairKeyDefault = Keys.X;
+        private static Keys cleanKeyDefault = Keys.C;
 
-        // initialise all keys - only this class can modify their values
-        public Keys spawnPedKey { get; private set; }
-        public Keys fixVehicleKey { get; private set; }
+        // keys/options - overridden if there's a config file
+        // default all keys
+        public Keys RepairKey { get; private set; } = repairKeyDefault;
+        public Keys CleanKey { get; private set; } = cleanKeyDefault;
+
+        // default options
+        public bool CleanOnRepair = true;
+        public bool RepairEngine = true; // can opt out and only fix vehicle cosmetics
+        public bool FrontWindowsDown = true; // after repairing vehicle, if the window isn't down the player needs to break the window to shoot again
+        public bool PartialCosmeticRepair = false; // only fix the damage to the body SHAPE - leave broken windows/lights and scratches
+
 
         public Configuration()
         {
-            readConfigurationFile();
+            ReadConfigurationFile();
         }
 
         // reads the config file in scripts to set keybindings etc.
-        private void readConfigurationFile()
+        private void ReadConfigurationFile()
         {
             if (File.Exists(configFile))
             {
                 config = ScriptSettings.Load(configFile);
-                spawnPedKey = config.GetValue<Keys>("KeyBindings", "spawnPed", Keys.H);
-                fixVehicleKey = config.GetValue<Keys>("KeyBindings", "fixVehicle", Keys.X);
+                RepairKey = config.GetValue<Keys>(sectionKeys, "RepairVehicle", repairKeyDefault);
+                CleanKey = config.GetValue<Keys>(sectionKeys, "CleanVehicle", cleanKeyDefault);
+
+                CleanOnRepair = config.GetValue<bool>(sectionOptions, "CleanOnRepair", true);
+                RepairEngine = config.GetValue<bool>(sectionOptions, "RepairEngine", true);
+                FrontWindowsDown = config.GetValue<bool>(sectionOptions, "FrontWindowsDown", true);
+                PartialCosmeticRepair = config.GetValue<bool>(sectionOptions, "PartialCosmeticRepair", false);
             }
-            else
-            {
-                UI.ShowSubtitle("Could not load " + configFile + ", file has now been created in /scripts");
-                CreateSettingsFile();
-            }
+
+            // OPTIONAL - automatically generate a config file template if no file exists?
+            // else
+            // {
+            //     UI.ShowSubtitle("Could not load " + configFile + ", file has now been created in /scripts");
+            //     CreateSettingsFile();
+            // }
         }
 
+        /*
         // creates a template file for the config & keys
         private void CreateSettingsFile()
         {
@@ -48,8 +74,6 @@ namespace ModTemplate
                 sw.WriteLine("fixVehicle = X; here is a comment"); // TODO
             }
         }
-
+        */
     }
 }
-
-
---
+++ VehicleInstantRepair.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using GTA;
+using GTA.Math;
+using GTA.Native;
+using System.Windows.Forms;
+using System.Drawing;
+using NativeUI;
+using System.IO;
+
+
+namespace VehicleInstantRepair
+{
+    public class VehicleInstantRepair : Script
+    {
+
+        Configuration Config;
+
+        public VehicleInstantRepair()
+        {
+            Config = new Configuration(); // load from config and set keys
+            KeyDown += onKeyDown;
+
+            UI.Notify("script loaded");
+        }
+
+        // executed when a key is pressed in game
+        private void onKeyDown(object sender, KeyEventArgs eventArgs)
+        {
+            // if (eventArgs.KeyCode == Config.RepairKey) { RepairVehicle(); }
+            if (eventArgs.KeyCode == Config.RepairKey) { RepairVehicle(); }
+            if (eventArgs.KeyCode == Config.CleanKey) { damageVehicle(); }
+            if (eventArgs.KeyCode == Keys.Z) { spawnVehicle(); }
+        }
+
+
+
+
+        private void spawnVehicle()
+        {
+            Ped player = Game.Player.Character;
+
+            Vehicle vehicle = World.CreateVehicle(VehicleHash.Futo, player.Position + player.ForwardVector * 3.0f, player.Heading + 90);
+            
+            vehicle.SetMod(VehicleMod.Horns, 1000, false);
+            vehicle.CanTiresBurst = false;
+            vehicle.RollDownWindows();
+            vehicle.NumberPlate = "hello";
+            // vehicle.IsInvincible = true;
+            vehicle.PlaceOnGround();
+
+            UI.Notify(vehicle.GetHashCode().ToString());
+            //vehicle.IsInvincible
+
+            Game.Player.Character.SetIntoVehicle(vehicle, VehicleSeat.Driver);
+        }
+
+
+        private void RepairVehicle()
+        {
+
+            Ped player = Game.Player.Character;
+
+            if (player.IsInVehicle())
+            {
+                Vehicle vehicle = player.CurrentVehicle;
+
+                float engineHealth = vehicle.EngineHealth;
+
+                // Player.CurrentVehicle.EngineHealth = 100; // 1000 = 100%
+                // player.CurrentVehicle.IsDriveable = true;
+                // player.CurrentVehicle.Health = 1000;
+                // player.CurrentVehicle.IsDriveable = true;
+                // player.CurrentVehicle.EngineHealth = 10;
+
+                if (Config.CleanOnRepair) { vehicle.DirtLevel = 0; }
+
+                if (!Config.PartialCosmeticRepair)
+                {
+                    player.CurrentVehicle.Repair(); // full repair - engine health & cosmetics
+                } 
+                else
+                {
+                    // repair major deformations - not windows or scratches etc.
+                    Function.Call(Hash.SET_VEHICLE_DEFORMATION_FIXED, player.CurrentVehicle, true);
+                }
+
+                if (Config.FrontWindowsDown)
+                {
+                    vehicle.RollDownWindow(VehicleWindow.FrontLeftWindow);
+                    vehicle.RollDownWindow(VehicleWindow.FrontRightWindow);
+                }
+
+                // apply the previous health before repairing vehicle
+                if (!Config.RepairEngine) { vehicle.EngineHealth = engineHealth; }
+
+                // if water damaged, bring back to life!
+                if (vehicle.IsDead) { player.CurrentVehicle.EngineRunning = true; }
+
+
+                /*
+                player.CurrentVehicle.FixWindow(VehicleWindow.BackLeftWindow);
+                player.CurrentVehicle.FixWindow(VehicleWindow.BackRightWindow);
+                player.CurrentVehicle.FixWindow(VehicleWindow.FrontLeftWindow);
+                player.CurrentVehicle.FixWindow(VehicleWindow.BackRightWindow);
+                */
+
+                // Function.Call(Hash.SET_VEHICLE_FIXED)
+                // Function.Call(Hash.SET_VEHICLE_DAMAGE, player.CurrentVehicle, 0);
+                // player.CurrentVehicle.LeftIndicatorLightOn
+                
+
+                UI.Notify(vehicle.FriendlyName + "dirt level: " + vehicle.DirtLevel.ToString());
+                // UI.Notify(Player.CurrentVehicle.Acceleration.ToString());
+                // UI.Notify(player.CurrentVehicle.DisplayName); // mod spawn name or default name
+            }
+        }
+
+        /*
+        // handy for testing
+        private void damageVehicle()
+        {
+            Ped player = Game.Player.Character;
+
+            if (player.IsInVehicle())
+            {
+                player.CurrentVehicle.EngineHealth = 1;
+                player.CurrentVehicle.DirtLevel = 0;
+            }
+        }
+        */
+    }
+}
